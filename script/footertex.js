ad131728c3aaf17714f4406c6cd0d5a319ea1dd9
mejianica <mejianica@hotmail.com>
2025-04-19 09:57:37 -0600

    color footer

---
+++ script/footertex.js
@@ -0,0 +1,8 @@
+let ver = document.querySelector(".texto");
+ver.addEventListener("mouseover",cambiaraorangered);
+
+function cambiaraorangered() {
+
+  document.getElementById("texto").style.color = "orangered";
+
+}
