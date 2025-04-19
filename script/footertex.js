5e696d8cbdddf20d9b4628f957284d8263ba85b8
mejianica <mejianica@hotmail.com>
2025-04-19 10:48:06 -0600

    error de logica de pollo

--- script/footertex.js
+++ script/footertex.js
@@ -1,8 +1,14 @@
 let ver = document.querySelector("#texto");
-ver.addEventListener("mouseover",cambiaraorangered);
 
 function cambiaraorangered() {
 
   document.getElementById("texto").style.color = "orangered";
-
+  
 }
+ver.addEventListener("mouseover", cambiaraorangered);
+
+
+ver.removeEventListener("mouseout",cambiaraorangered );
+
+
+
